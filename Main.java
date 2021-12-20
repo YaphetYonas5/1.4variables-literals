52a921a66568692d1e0159e50d71029765a3241c
Yaphet Yonas <85519133+YaphetYonas5@users.noreply.github.com>
2021-12-20 02:37:49 +0000

    1.4variables/literals commit 1

---
+++ Main.java
@@ -0,0 +1,24 @@
+/**
+ *  This program demonstrates
+ *  how to use variables in a program
+ */
+public class Main
+{
+   public static void main(String[] args)
+   {
+      // Declare variables to hold data.
+      int age;
+      String name;
+      String grade;
+
+      // Assign values to variables.
+      age = 14;
+      name   = "yaphet";
+      grade  = "freshman";
+
+      // Display the message
+      System.out.println("Your age is " + age);
+      System.out.println("Your name is " + name);
+      System.out.println("You are a " + grade);
+   }
+}
